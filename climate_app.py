16efe7754aea5dbabd251ebe496e999ff0ee6247
Hernan Hernandez <hhernandez0924@gmail.com>
2019-10-02 21:49:46 -0400

    updated climate_app

--- climate_app.py
+++ climate_app.py
@@ -29,11 +29,11 @@ climate_app = Flask(__name__)
 @climate_app.route("/")
 def home():
     return(f"Available routes<br>"
-           f"route: /api/v1.0/precipitation , def: Precipitation data<br>"
-           f"route: /api/v1.0/stations , def: List of Stations<br>"
-           f"route: /api/v1.0/tobs , def: Date and temp observations from a year from the last data point<br>"
-           f"route: /api/v1.0/startdate , def: When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date<br>"
-           f"route: /api/v1.0/startdate/enddate , def: Minimum temperature, the average temperature, and the max temperature for a given start or start-end range. Or, When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.")
+           f"route: /api/v1.0/precipitation<br>"
+           f"route: /api/v1.0/stations<br>"
+           f"route: /api/v1.0/tobs<br>"
+           f"route: /api/v1.0/2016-07-31<br>"
+           f"route: /api/v1.0/2011-01-01/2011-12-31")
 
 @climate_app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -41,9 +41,42 @@ def precipitation():
     precipitation_data = session.query(Measurement.date, Measurement.prcp).\
     filter(Measurement.date >= '2016-08-23').filter(Measurement.date <= '2017-08-23').all()
 
-    precipitation_data_dic = dict(precipitation_data)
+    precipitation_data_dict = dict(precipitation_data)
 
-    return jsonify(precipitation_data_dic)
+    return jsonify(precipitation_data_dict)
+
+@climate_app.route("/api/v1.0/stations")
+def stations():
+
+    stations = session.query(Station.name).all()
+    station_list = list(np.ravel(stations))
+
+    return jsonify(station_list)
+
+@climate_app.route("/api/v1.0/tobs")
+def temp_observ():
+
+    temps_observ = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date >= '2016-08-23').filter(Measurement.date <= '2017-08-23')
+
+    temps_observ_list = list(temps_observ)
+
+    return jsonify(temps_observ_list)
+
+@climate_app.route("/api/v1.0/<start>")
+def start(start=None):
+
+    from_start = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).group_by(Measurement.date).all()
+    from_start_list=list(from_start)
+    
+    return jsonify(from_start_list)
+
+@climate_app.route("/api/v1.0/<start>/<end>")
+def start_end(start=None, end=None):
+    
+    between_dates = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()
+    between_dates_list=list(between_dates)
+
+    return jsonify(between_dates_list)
 
 if __name__ == "__main__":
     climate_app.run(debug=True)
